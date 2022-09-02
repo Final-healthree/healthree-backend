3c62297d47cf2fd7073d5acb8800c0137e67e24f
phenomenonlee <flejdud8@naver.com>
2022-09-02 19:48:46 +0900

    메인페이지_등록(작심삼일 등록) API 및 메인페이지_작심삼일 등록 ( goal 및 날짜 값 보내주는 api) API 기능 구현 완료

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -4,5 +4,6 @@ import * as main_controllers from "../controllers/main.controllers.js";
 const router = express.Router();
 
 router.post("/register", main_controllers.main_register);
+router.get("/goal_day/:day", main_controllers.find_goal_day);
 
 export default router;
