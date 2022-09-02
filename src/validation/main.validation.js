dbd407e95421d8ef1565d2c61a8e16938d5e72bc
phenomenonlee <flejdud8@naver.com>
2022-09-02 21:44:35 +0900

    git validation folder upload

--- src/validation/main.validation.js
+++ src/validation/main.validation.js
@@ -1,9 +1,14 @@
 import Ing from "../models/ing.js";
 
 export const check_video = async (user_id) => {
-    const check_video = await Ing.findOne({ where: user_id });
-    if (check_video.video_one || check_video.video_two || check_video.video_three) {
-        return false;
+    try {
+        const check_video = await Ing.findOne({ where: { user_id } });
+        console.log(check_video);
+        if (check_video.video_one || check_video.video_two || check_video.video_three) {
+            return false;
+        }
+        return true;
+    } catch (error) {
+        return { error };
     }
-    return true;
 };
