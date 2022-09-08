068cc6938a2d1dae32b9bfb3d61e061cbbf9c1cd
phenomenonlee <flejdud8@naver.com>
2022-09-08 17:55:37 +0900

    main.service.test.js 테스트 코드 작성 완료

--- test/main_repository.test.js
+++ test/main_repository.test.js
@@ -14,4 +14,20 @@ describe("main_repository , main_register", () => {
     const day2 = "2";
     const day3 = "3";
     const goal_name = "목표";
+
+    const model_goal = Goal.create;
+
+    test("post api/main/register /// 성공시, Goal model 한번 호출", async () => {
+        await main_register(user_id, day1, day2, day3, goal_name);
+
+        expect(model_goal).toBeCalledTimes(1);
+        model_goal.mockClear();
+    });
+
+    test("post api/main/register /// 실패시, Goal 모델 호출 안 함", async () => {
+        model_goal.mockReturnValue(Promise.reject());
+
+        await main_register(user_id, day1, day2, day3, goal_name);
+        expect(model_goal).toBeCalledTimes(0);
+    });
 });
