f9fceb599b21c9da6cb6090908655bc08b4fa6d7
waveinyu <waveinyu@gmail.com>
2022-09-12 15:46:09 +0900

    Refactoring `get_my_calendar`, `get_my_videos`, `share_my_video`

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -4,68 +4,45 @@ import Post from "../models/post.js";
 import { Op } from "sequelize";
 
 const get_my_calendar = async (user_id) => {
-    try {
-        const user_info = await User.findOne({
-            where: { user_id },
-            attributes: ["nickname", "profile_image"],
-            include: {
-                model: Goal,
-                attributes: [
-                    "goal_id",
-                    "status",
-                    "day1",
-                    "day2",
-                    "day3",
-                    "video1",
-                    "video2",
-                    "video3",
-                ],
-            },
-        });
-        return user_info;
-    } catch (err) {
-        throw err;
-    }
+    return await User.findOne({
+        where: { user_id },
+        attributes: ["nickname", "profile_image"],
+        include: {
+            model: Goal,
+            attributes: ["goal_id", "status", "day1", "day2", "day3", "video1", "video2", "video3"],
+        },
+    });
 };
 
 const get_my_videos = async (user_id, page_count, page) => {
-    try {
-        const videos = await Goal.findAll({
-            where: { user_id, status: "success", final_video: { [Op.ne]: null } },
-            offset: page_count * (page - 1),
-            limit: page_count,
-            attributes: ["goal_name", "day1", "day3", "final_video"],
-        });
-        return videos;
-    } catch (err) {
-        throw err;
-    }
+    return await Goal.findAll({
+        where: { user_id, status: "success", final_video: { [Op.ne]: null } },
+        offset: page_count * (page - 1),
+        limit: page_count,
+        attributes: ["goal_name", "day1", "day3", "final_video"],
+    });
 };
 
 const share_my_video = async (user_id, goal_id) => {
-    try {
-        const share_video = await Goal.update(
-            {
-                is_social: true,
+    await Goal.update(
+        {
+            is_social: true,
+        },
+        {
+            where: {
+                user_id,
+                goal_id,
+                video1: { [Op.ne]: null },
+                video2: { [Op.ne]: null },
+                video3: { [Op.ne]: null },
+                final_video: { [Op.ne]: null },
             },
-            {
-                where: {
-                    user_id,
-                    goal_id,
-                    video1: { [Op.ne]: null },
-                    video2: { [Op.ne]: null },
-                    video3: { [Op.ne]: null },
-                    final_video: { [Op.ne]: null },
-                },
-            },
-        );
+        },
+    );
 
-        const create_video_post = await Post.create({
-            goal_id,
-        });
-    } catch (err) {
-        throw err;
-    }
+    await Post.create({
+        goal_id,
+    });
 };
 
 export { get_my_calendar, get_my_videos, share_my_video };
--- src/services/users.service.js
+++ src/services/users.service.js
@@ -2,95 +2,77 @@ import * as users_repository from "../repositories/users.repository.js";
 import Goal from "../models/goal.js";
 
 const get_my_calendar = async (user_id) => {
-    try {
-        const user_info = await users_repository.get_my_calendar(user_id);
-        const user = {
-            nickname: user_info.nickname,
-            profile_image: user_info.profile_image,
+    const user_info = await users_repository.get_my_calendar(user_id);
+    const user = {
+        nickname: user_info.nickname,
+        profile_image: user_info.profile_image,
+    };
+    const distinguish = user_info.Goals.map((d, index) => {
+        return {
+            goal_id: d.goal_id,
+            status: d.status,
+            day1: d.day1,
+            day2: d.day2,
+            day3: d.day3,
+            video1: d.video1,
+            video2: d.video2,
+            video3: d.video3,
         };
-        const user_goals = user_info.Goals;
-        const distinguish = user_goals.map((d, index) => {
-            return {
-                goal_id: d.goal_id,
-                status: d.status,
-                day1: d.day1,
-                day2: d.day2,
-                day3: d.day3,
-                video1: d.video1,
-                video2: d.video2,
-                video3: d.video3,
-            };
-        });
+    });
 
-        const success = [];
-        const fail = [];
-        for (let i = 0; i < distinguish.length; i++) {
-            if (
-                distinguish[i].status === "success" &&
-                distinguish[i].video1 !== null &&
-                distinguish[i].video2 !== null &&
-                distinguish[i].video3 !== null
-            ) {
-                success.push({
-                    goal_id: distinguish[i].goal_id,
-                    day1: distinguish[i].day1,
-                    day2: distinguish[i].day2,
-                    day3: distinguish[i].day3,
-                });
-            }
-
-            if (distinguish[i].status === "fail") {
-                if (
-                    distinguish[i].video1 !== null &&
-                    distinguish[i].video2 === null &&
-                    distinguish[i].video3 === null
-                ) {
-                    fail.push({ goal_id: distinguish[i].goal_id, day1: distinguish[i].day1 });
-                }
-                if (
-                    distinguish[i].video1 !== null &&
-                    distinguish[i].video2 !== null &&
-                    distinguish[i].video3 === null
-                ) {
-                    fail.push({
-                        goal_id: distinguish[i].goal_id,
-                        day1: distinguish[i].day1,
-                        day2: distinguish[i].day2,
-                    });
-                }
-            }
+    const date = [];
+    for (let i = 0; i < distinguish.length; i++) {
+        if (
+            distinguish[i].status === "success" &&
+            distinguish[i].video1 !== null &&
+            distinguish[i].video2 !== null &&
+            distinguish[i].video3 !== null
+        ) {
+            date.push({
+                goal_id: distinguish[i].goal_id,
+                date: [distinguish[i].day1, distinguish[i].day2, distinguish[i].day3],
+            });
         }
 
-        return { nickname: user.nickname, profile: user.profile_image, success, fail };
-    } catch (err) {
-        throw err;
+        if (distinguish[i].status === "fail") {
+            if (
+                distinguish[i].video1 !== null &&
+                distinguish[i].video2 === null &&
+                distinguish[i].video3 === null
+            ) {
+                date.push({ goal_id: distinguish[i].goal_id, date: distinguish[i].day1 });
+            }
+            if (
+                distinguish[i].video1 !== null &&
+                distinguish[i].video2 !== null &&
+                distinguish[i].video3 === null
+            ) {
+                date.push({
+                    goal_id: distinguish[i].goal_id,
+                    date: [distinguish[i].day1, distinguish[i].day2],
+                });
+            }
+        }
     }
+    return { nickname: user.nickname, profile: user.profile_image, date };
 };
 
 const get_my_videos = async (user_id, page_count, page) => {
-    try {
-        const videos = await users_repository.get_my_videos(user_id, page_count, page);
-        const video_list = videos.map((v, index) => {
-            return {
-                goal_name: v.goal_name,
-                day1: v.day1,
-                day3: v.day3,
-                final_video: v.final_video,
-            };
-        });
+    const videos = await users_repository.get_my_videos(user_id, page_count, page);
+    const video_list = videos.map((v, index) => {
+        return {
+            goal_name: v.goal_name,
+            day1: v.day1,
+            day3: v.day3,
+            final_video: v.final_video,
+        };
+    });
 
-        return { video_list };
-    } catch (err) {
-        throw err;
-    }
+    return { video_list };
 };
 
 const share_my_video = async (user_id, goal_id) => {
-    try {
-        await users_repository.share_my_video(user_id, goal_id);
-    } catch (err) {
-        throw err;
-    }
+    await users_repository.share_my_video(user_id, goal_id);
 };
 
 export { get_my_calendar, get_my_videos, share_my_video };
