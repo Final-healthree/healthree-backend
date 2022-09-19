df864bf0c2087cb21bf0849b8194bb1a2dcf72cd
waveinyu <waveinyu@gmail.com>
2022-09-20 02:13:29 +0900

    Modify `models/goal`, `models/video`

--- src/models/index.js
+++ src/models/index.js
@@ -5,6 +5,7 @@ import sequelize from "./sequelize.js";
 // 유저, 포스트, 댓글, 좋아요 임포트
 import User from "./user.js";
 import Goal from "./goal.js";
+import Video from "./video.js";
 import Post from "./post.js";
 import Comment from "./comment.js";
 
@@ -14,18 +15,21 @@ const db = {};
 db.sequelize = sequelize;
 db.User = User;
 db.Goal = Goal;
+db.Video = Video;
 db.Post = Post;
 db.Comment = Comment;
 
 // init한 부분 설정
 User.init(sequelize);
 Goal.init(sequelize);
+Video.init(sequelize);
 Post.init(sequelize);
 Comment.init(sequelize);
 
 // 외래키 관계부분
 User.associate(db);
 Goal.associate(db);
+Video.associate(db);
 Post.associate(db);
 Comment.associate(db);
 
