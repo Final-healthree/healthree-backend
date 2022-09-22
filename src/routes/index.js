79fb149804098fd03137db5018d0cdf1d1684717
waveinyu <waveinyu@gmail.com>
2022-09-22 22:00:55 +0900

    Set `routes`

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,13 @@ const router = express.Router();
 import auth_router from "./auth.routes.js";
 import goal_router from "./goal.routes.js";
 import video_router from "./video.routes.js";
-import posts_router from "./post.routes.js";
-import comments_router from "./comments.routes.js";
+import post_router from "./post.routes.js";
+import comment_router from "./comment.routes.js";
 
 router.use("/auth", auth_router);
 router.use("/goals", goal_router);
 router.use("/videos", video_router);
-router.use("/posts", video_router);
-router.use("/comments", video_router);
+router.use("/posts", post_router);
+router.use("/comments", comment_router);
 
 export default router;
