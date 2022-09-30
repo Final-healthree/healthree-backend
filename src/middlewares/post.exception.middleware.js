7a812e907f0a671eae252f3479dc63b7af133981
waveinyu <waveinyu@gmail.com>
2022-09-30 19:32:09 +0900

    Modify status codes

--- src/middlewares/post.exception.middleware.js
+++ src/middlewares/post.exception.middleware.js
@@ -12,13 +12,13 @@ export const is_accessible_post = async (req, res, next) => {
         });
 
         if (!is_exist_post) {
-            return res.status(400).json({ success: false, message: "존재하지 않는 게시글입니다." });
+            return res.status(404).json({ success: false, message: "존재하지 않는 게시글입니다." });
         }
 
         next();
     } catch (error) {
         return res
-            .status(400)
+            .status(500)
             .json({ success: false, message: `${error.name} , ${error.message}` });
     }
 };
@@ -34,14 +34,14 @@ export const is_available_delete = async (req, res, next) => {
             include: { model: Goal, attributes: ["user_id"] },
         });
         if (user_id !== is_writer.Goal.user_id) {
-            return res.status(400).json({ success: false, message: "작성자가 아닙니다." });
+            return res.status(403).json({ success: false, message: "작성자가 아닙니다." });
         }
 
         req.goal_id = is_writer?.goal_id;
         next();
     } catch (error) {
         return res
-            .status(400)
+            .status(500)
             .json({ success: false, message: `${error.name} , ${error.message}` });
     }
 };
@@ -52,12 +52,12 @@ export const is_available_like = async (req, res, next) => {
         const is_exist_post = await Post.findOne({ where: { post_id } });
 
         if (!is_exist_post)
-            return res.status(400).json({ success: false, message: "존재하지 않는 게시물입니다." });
+            return res.status(404).json({ success: false, message: "존재하지 않는 게시물입니다." });
 
         next();
     } catch (error) {
         return res
-            .status(400)
+            .status(500)
             .json({ success: false, message: `${error.name} , ${error.message}` });
     }
 };
