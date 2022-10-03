6ddf8d789b9cd665138482092f1bd48ea22f2fde
waveinyu <waveinyu@gmail.com>
2022-10-03 11:50:24 +0900

    Add remark, Modify auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,6 +5,7 @@ import User from "../models/user.js";
 
 dotenv.config({ path: path.resolve(".env") });
 
+// 로그인 후 사용자 인증 정보를 저장하는 인증 미들웨어
 export const auth_middleware = async (req, res, next) => {
     const { authorization } = req.headers;
     const [auth_type, auth_value] = (authorization || " ").split(" ");
@@ -15,24 +16,16 @@ export const auth_middleware = async (req, res, next) => {
 
     try {
         const user_info = jwt.verify(auth_value, process.env.JWT_SECRET);
-        const { user_id } = user_info.payload;
-
-        const is_exist_user = await User.findOne({ where: { user_id } });
-        if (!is_exist_user)
-            return res.status(400).json({ success: false, message: "존재하지 않는 유저입니다." });
-
-        if (is_exist_user.user_id !== user_id)
-            return res.status(400).json({ success: false, message: "존재하지 않는 유저입니다." });
 
         res.locals.user_id = user_info.payload.user_id;
         res.locals.nickname = user_info.payload.nickname;
         res.locals.profile_image = user_info.payload.profile_image;
 
         next();
-    } catch (err) {
-        if (err.name === "TokenExpiredError")
+    } catch (error) {
+        if (error.name === "TokenExpiredError")
             return res.status(419).json({ success: false, message: "토큰이 만료되었습니다." });
-        if (err.name === "jsonwebtokenError")
+        if (error.name === "jsonwebtokenError")
             return res.status(401).json({ success: false, message: "토큰이 유효하지 않습니다." });
     }
 };
--- src/middlewares/post.exception.middleware.js
+++ src/middlewares/post.exception.middleware.js
@@ -3,13 +3,11 @@ import User from "../models/user.js";
 import Goal from "../models/goal.js";
 import Like from "../models/like.js";
 
+// 접근 가능한 포스트인지 확인
 export const is_accessible_post = async (req, res, next) => {
     try {
         const { post_id } = req.params;
-
-        const is_exist_post = await Post.findOne({
-            where: { post_id },
-        });
+        const is_exist_post = await Post.findOne({ where: { post_id } });
 
         if (!is_exist_post) {
             return res.status(404).json({ success: false, message: "존재하지 않는 게시글입니다." });
@@ -23,6 +21,7 @@ export const is_accessible_post = async (req, res, next) => {
     }
 };
 
+// 해당 유저가 삭제 가능한 포스트인지 작성자 확인
 export const is_available_delete = async (req, res, next) => {
     try {
         const { user_id } = res.locals;
@@ -38,22 +37,6 @@ export const is_available_delete = async (req, res, next) => {
         }
 
         req.goal_id = is_writer?.goal_id;
-        next();
-    } catch (error) {
-        return res
-            .status(500)
-            .json({ success: false, message: `${error.name} , ${error.message}` });
-    }
-};
-
-export const is_available_like = async (req, res, next) => {
-    try {
-        const { post_id } = req.params;
-        const is_exist_post = await Post.findOne({ where: { post_id } });
-
-        if (!is_exist_post)
-            return res.status(404).json({ success: false, message: "존재하지 않는 게시물입니다." });
-
         next();
     } catch (error) {
         return res
